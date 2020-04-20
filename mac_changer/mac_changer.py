67ddb9c0449994324a759cc3dec5489ac944db2d
root <root@itstimetogetout.orisit>
2020-04-20 21:05:15 +0530

    bruh

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -4,7 +4,7 @@ import subprocess
 
 subprocess.call('ifconfig', shell=True)
 subprocess.call('ifconfig wlan0 down', shell=True)
-subprocess.call('ifconfig wlano hw ether 00:11:22:33:44:55', shell=True)
+subprocess.call('ifconfig wlan0 hw ether 00:11:22:33:44:55', shell=True)
 subprocess.call('ifconfig wlan0 up', shell=True)
 subprocess.call('ifconfig', shell=True)
 
