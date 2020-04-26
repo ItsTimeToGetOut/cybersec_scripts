a045d6afa6785b1cab28150e4b95914a7d097930
Shubhendu Singh <shubhendusingh15@gmail.com>
2020-04-26 15:46:57 +0530

    update

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,9 +2,13 @@
 
 import subprocess
 
-subprocess.call('ifconfig', shell=True)
-subprocess.call('ifconfig wlan0 down', shell=True)
-subprocess.call('ifconfig wlan0 hw ether 00:11:22:33:44:55', shell=True)
-subprocess.call('ifconfig wlan0 up', shell=True)
-subprocess.call('ifconfig', shell=True)
+interface = input('interface > ')
+new_mac = input('new MAC > ')
+
+print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
+#will only work when the first hex is an even number
+
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
 
